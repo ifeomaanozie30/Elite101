67425c67eb94e031c122ee01739b4f439a67c0e6
ifeomaanozie30 <143964009+ifeomaanozie30@users.noreply.github.com>
2023-09-23 20:02:00 +0000

    Final draft of my code

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-print('Welcome to the ChatBot!')
-input('What is your name and age?\n')
-print('Hi user_name How can I help you?')
+#I was not entirely sure what happened to my console and it would not let me see what exactly I was coding when used to work.
+def user_name():
+ #user_name()
+ print('Hello! I am your chatbot')
+ user_name = input('What is your name?\n')
+ return 
+
+def user_age():
+  #user_name()
+  user_name = input('Hi! Nice to meet you {}. How old are you?'.format(user_name))
+
+def display_options():
+  print('\nHow can I assist you today?'.format(user_name))
+  print('1. Provide information')
+  print('2. Ask a question')
+  print('3. Exit')
+
+  while True:
+    display_options()
+    choice = input('Please select 1, 2, or, 3')
+    if choice == '1':
+      print("You have selected 'Provide information'.")
+      #Add code for whatever information they might need
+    elif choice == '2':
+      print("You have selected 'Ask a question.'")
+      #Add code to ask what their question is and then answer it
+    elif choice == '3':
+      print("Bye!{} Please come again soon!" .format(user_name))
+      break
+      #Only used if the user wants to leave
+    else:
+     print("I'm sorry that's not a choice, Please select 1, 2, or 3.")
+
+    
+    
+  
+
+ 
+
+
+
